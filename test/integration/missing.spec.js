d7a7deee5fe96e73daf23d2fbdb39ed36044c643
Sam Mason <samm@lostmy.name>
2016-12-04 14:20:09 +0000

    Add test for 404

--- test/integration/missing.spec.js
+++ test/integration/missing.spec.js
@@ -2,9 +2,9 @@ const request = require('supertest')
 const app = require('../../index')
 
 describe('/random/non/existing/route', function() {
-  it('should return one beer at random', function (done) {
+  it('should return a 404 object', function (done) {
     request(app)
-      .get('/beers/random')
+      .get('/random/non/existing/route')
       .end(function(err, res) {
         res.statusCode.should.equal(404)
         res.should.be.json()
