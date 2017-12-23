3a429b3d3e1505cc8ddfcbeb8b3dff585e567c68
Sam Mason <sam.mason@rapha.cc>
2017-12-23 18:16:18 +0000

    Remove sentry code and update db

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const system = require('./system')
-const express = require('express')
-const sentry = require('./lib/sentry')
-const useCors = require('./lib/cors')
-const rateLimit = require('./lib/rateLimit')
-const { errorHandler, notFoundError } = require('./lib/errorHandler')
+const system = require("./system");
+const express = require("express");
+const useCors = require("./lib/cors");
+const rateLimit = require("./lib/rateLimit");
+const { errorHandler, notFoundError } = require("./lib/errorHandler");
 
-const app = express()
+const app = express();
 
-sentry.config()
+app.use(require("helmet")());
+app.use(require("express-validator")());
 
-app.use(sentry.reqHandler())
+app.use("/v2", useCors(), rateLimit, require("./routes"));
+app.use("*", (req, res, next) =>
+  next(notFoundError(`No endpoint found that matches '${req.originalUrl}'`))
+);
 
-app.use(require('helmet')())
-app.use(require('express-validator')())
+app.use(errorHandler);
 
-app.use('/v2', useCors(), rateLimit, require('./routes'))
-app.use('*', (req, res, next) => next(notFoundError(`No endpoint found that matches '${req.originalUrl}'`)))
+const server = app.listen(system.getPort(), function(error) {
+  if (error) throw error;
 
-app.use(sentry.errHandler())
+  process.once("SIGTERM", system.shutdown(server));
+  console.info(`Listening on http://localhost:${server.address().port}`);
+});
 
-app.use(errorHandler)
-
-const server = app.listen(system.getPort(), function (error) {
-  if (error) throw error
-
-  process.once('SIGTERM', system.shutdown(server))
-  console.info(`Listening on http://localhost:${server.address().port}`)
-})
-
-module.exports = app
+module.exports = app;
