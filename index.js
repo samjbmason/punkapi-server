36db203d9d79a568dc7e4de3868289599d72cd60
Sam Mason <sam.mason@rapha.cc>
2017-12-23 18:21:43 +0000

    Update prettier config

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const system = require("./system");
-const express = require("express");
-const useCors = require("./lib/cors");
-const rateLimit = require("./lib/rateLimit");
-const { errorHandler, notFoundError } = require("./lib/errorHandler");
+const system = require('./system')
+const express = require('express')
+const useCors = require('./lib/cors')
+const rateLimit = require('./lib/rateLimit')
+const { errorHandler, notFoundError } = require('./lib/errorHandler')
 
-const app = express();
+const app = express()
 
-app.use(require("helmet")());
-app.use(require("express-validator")());
+app.use(require('helmet')())
+app.use(require('express-validator')())
 
-app.use("/v2", useCors(), rateLimit, require("./routes"));
-app.use("*", (req, res, next) =>
+app.use('/v2', useCors(), rateLimit, require('./routes'))
+app.use('*', (req, res, next) =>
   next(notFoundError(`No endpoint found that matches '${req.originalUrl}'`))
-);
+)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const server = app.listen(system.getPort(), function(error) {
-  if (error) throw error;
+  if (error) throw error
 
-  process.once("SIGTERM", system.shutdown(server));
-  console.info(`Listening on http://localhost:${server.address().port}`);
-});
+  process.once('SIGTERM', system.shutdown(server))
+  console.info(`Listening on http://localhost:${server.address().port}`)
+})
 
-module.exports = app;
+module.exports = app
